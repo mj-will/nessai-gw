4c5e2987bef25c0be1dde195104f40a41df82ceb
Michael J. Williams <michaeljw1@googlemail.com>
2026-02-24 12:04:00 +0000

    TST: add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -75,7 +75,12 @@ def get_bilby_priors_and_likelihood():
         )
         for key in fixed_params:
             if key in injection_parameters:
-                priors[key] = injection_parameters[key]
+                priors[key] = bilby.core.prior.DeltaFunction(
+                    name=key,
+                    latex_label=key,
+                    unit=None,
+                    value=injection_parameters[key],
+                )
 
         waveform_generator = bilby.gw.WaveformGenerator(
             duration=4,
@@ -111,6 +116,7 @@ def get_bilby_gw_model(get_bilby_priors_and_likelihood) -> Callable:
         priors, likelihood = get_bilby_priors_and_likelihood(
             parameters, injection_parameters
         )
+        print(priors)
         return BilbyModel(priors=priors, likelihood=likelihood)
 
     return get_model
--- tests/test_reparameterisations/test_lisa.py
+++ tests/test_reparameterisations/test_lisa.py
@@ -6,30 +6,48 @@ from nessai.utils.testing import assert_structured_arrays_equal
 from nessai_gw.reparameterisations.lisa import LISAExtrinsicSymmetry
 
 
+@pytest.fixture(params=["eclipticlatitude", "sin_eclipticlatitude"])
+def beta_parameter_name(request):
+    return request.param
+
+
 @pytest.fixture
-def extrinsic_parameters():
+def extrinsic_parameters(beta_parameter_name):
     return [
         "eclipticlongitude",
-        "eclipticlatitude",
+        beta_parameter_name,
         "polarization",
         "iota",
     ]
 
 
 @pytest.fixture
-def extrinsic_prior_bounds():
+def extrinsic_prior_bounds(beta_parameter_name):
+    beta_bounds = (
+        [-np.pi / 2, np.pi / 2]
+        if beta_parameter_name == "eclipticlatitude"
+        else [-1.0, 1.0]
+    )
     return {
         "eclipticlongitude": [0, 2 * np.pi],
-        "eclipticlatitude": [-np.pi / 2, np.pi / 2],
+        beta_parameter_name: beta_bounds,
         "polarization": [0, np.pi],
         "iota": [0, np.pi],
     }
 
 
+@pytest.mark.parametrize("include_phase", [False, True])
 @pytest.mark.integration_test
 def test_extrinsic_reparam_invertible(
-    extrinsic_parameters, extrinsic_prior_bounds, rng, n_samples
+    extrinsic_parameters, extrinsic_prior_bounds, rng, n_samples, include_phase
 ):
+    if include_phase:
+        extrinsic_parameters = extrinsic_parameters + ["phase"]
+        extrinsic_prior_bounds = {
+            **extrinsic_prior_bounds,
+            "phase": [0, 2 * np.pi],
+        }
+
     reparam = LISAExtrinsicSymmetry(
         parameters=extrinsic_parameters,
         prior_bounds=extrinsic_prior_bounds,
@@ -50,3 +68,58 @@ def test_extrinsic_reparam_invertible(
 
     assert_structured_arrays_equal(x_re, x, atol=1e-14)
     np.testing.assert_equal(log_j_re, 0.0)
+
+
+def test_determine_modes_expected_indices(
+    extrinsic_parameters, extrinsic_prior_bounds
+):
+    reparam = LISAExtrinsicSymmetry(
+        parameters=extrinsic_parameters,
+        prior_bounds=extrinsic_prior_bounds,
+    )
+
+    n = 8
+    x = empty_structured_array(n, reparam.parameters)
+    x["eclipticlongitude"] = np.array(
+        [0.1, np.pi / 2 + 0.1, np.pi + 0.1, 3 * np.pi / 2 + 0.1] * 2
+    )
+    x[reparam.beta_parameter] = np.array([0.2] * 4 + [-0.2] * 4)
+    x["polarization"] = 0.5
+    x["iota"] = 1.0
+
+    mode_ids = reparam.determine_modes(x)
+
+    np.testing.assert_array_equal(mode_ids.long_num, [0, 1, 2, 3, 0, 1, 2, 3])
+    np.testing.assert_array_equal(mode_ids.lat_num, [1, 1, 1, 1, 0, 0, 0, 0])
+    np.testing.assert_array_equal(mode_ids.phase_num, np.zeros(n, dtype=int))
+    np.testing.assert_array_equal(mode_ids.index, [4, 5, 6, 7, 0, 1, 2, 3])
+
+
+def test_unfold_modes_with_phase_indices(
+    extrinsic_parameters, extrinsic_prior_bounds
+):
+    parameters = extrinsic_parameters + ["phase"]
+    prior_bounds = {**extrinsic_prior_bounds, "phase": [0, 2 * np.pi]}
+
+    reparam = LISAExtrinsicSymmetry(
+        parameters=parameters,
+        prior_bounds=prior_bounds,
+    )
+
+    mode_index = np.arange(16)
+    mode_ids = reparam.unfold_modes(mode_index)
+
+    np.testing.assert_array_equal(mode_ids.phase_num, [0] * 8 + [1] * 8)
+    np.testing.assert_array_equal(
+        mode_ids.long_num,
+        [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
+    )
+    np.testing.assert_array_equal(
+        mode_ids.lat_num,
+        [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+    )
+
+    reconstructed = (
+        mode_ids.long_num + 4 * mode_ids.lat_num + 8 * mode_ids.phase_num
+    )
+    np.testing.assert_array_equal(reconstructed, mode_index)
